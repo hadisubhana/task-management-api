e5edc08fa976ba98768a9cb1b1e3d1c441223d67
hadisubhana <hadisubhana@gmail.com>
2026-03-13 19:22:17 +0700

    feat(task): add task CRUD API

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,38 +14,63 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::latest()->paginate(10);
+
+        return response()->json($tasks);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create([
+            'project_id' => $request->project_id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'status' => 'todo'
+        ]);
+
+        return response()->json([
+            'message' => 'Task created',
+            'data' => $task
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return response()->json($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Task $task)
     {
-        //
+        $task->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'status' => $request->status
+        ]);
+
+        return response()->json([
+            'message' => 'Task updated',
+            'data' => $task
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Task deleted'
+        ]);
     }
 }
