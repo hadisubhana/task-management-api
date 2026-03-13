d2b39e83cdda6aabcaf919d3e1ddfefc96c70cd2
hadisubhana <hadisubhana@gmail.com>
2026-03-13 19:05:47 +0700

    add project CRUD API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
@@ -12,38 +15,63 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::where('user_id', Auth::id())
+            ->latest()
+            ->paginate(10);
+
+        return response()->json($projects);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request)
     {
-        //
+        $project = Project::create([
+            'user_id' => Auth::id(),
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+
+        return response()->json([
+            'message' => 'Project created',
+            'data' => $project
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Project $project)
     {
-        //
+        return response()->json($project);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Project $project)
     {
-        //
+        $project->update([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+
+        return response()->json([
+            'message' => 'Project updated',
+            'data' => $project
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return response()->json([
+            'message' => 'Project deleted'
+        ]);
     }
 }
