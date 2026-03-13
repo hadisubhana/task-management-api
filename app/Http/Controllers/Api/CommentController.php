45de09b5597bd3b4e4774eee72887f354ededa9e
hadisubhana <hadisubhana@gmail.com>
2026-03-13 19:30:44 +0700

    feat(comment): add comment API

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,24 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(string $taskId)
     {
-        //
+
+        $comments = Comment::where('task_id', $taskId)
+            ->with('user')
+            ->latest()
+            ->get();
+
+        return response()->json($comments);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCommentRequest $request)
     {
-        //
+        $comment = Comment::create([
+            'task_id' => $request->task_id,
+            'user_id' => Auth::id(),
+            'content' => $request->content
+        ]);
+
+        return response()->json([
+            'message' => 'Comment created',
+            'data' => $comment
+        ]);
     }
 
     /**
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = [
+        'task_id',
+        'user_id',
+        'content'
+    ];
+
     public function task()
     {
         return $this->belongsTo(Task::class);
